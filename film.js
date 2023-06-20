0d764e85d02fecb5433477cae5ae811ad646f6be
Timofei Belov <timbe2017@yandex.ru>
2023-06-20 21:49:03 +0300

    add design for film card

--- film.js
+++ film.js
@@ -5,19 +5,21 @@ const filmCardNode = document.querySelector(".js-film-card");
 
 const renderFilmCard = (filmInfo) => {
   filmCardNode.innerHTML = `
-    <div class="col">
-        <img class="fim_card__img" src=${filmInfo.Poster}>
-    </div>
-    <div class="col">
-        <p class="film__title>${filmInfo.Title}</p>
-        <p class="film__year>Год:<span>${filmInfo.Year}</span></p>
-        <p class="film__rating>Рейтинг:<span>${filmInfo.imdbRating}</span></p>
-        <p class="film__released>Дата выхода:<span>${filmInfo.Released}</span></p>
-        <p class="film__runtime>Продолжительность:<span>${filmInfo.Runtime}</span></p>
-        <p class="film__genre>Жанр:<span>${filmInfo.Genre}</span></p>
-        <p class="film__director>Режиссер:<span>${filmInfo.Director}</span></p>
-        <p class="film__writer>Сценарий:<span>${filmInfo.Writer}</span></p>
-        <p class="film__actors>Актеры:<span>${filmInfo.Actors}</span></p>
+    <div class="film_item">
+        <div class="col">
+            <img class="film_card__img" src=${filmInfo.Poster}>
+        </div>
+        <div class="col">
+            <p class="film__title">${filmInfo.Title}</p>
+            <p class="film__description">Год:<span>${filmInfo.Year}</span></p>
+            <p class="film__description">Рейтинг:<span>${filmInfo.imdbRating}</span></p>
+            <p class="film__description">Дата выхода:<span>${filmInfo.Released}</span></p>
+            <p class="film__description">Продолжительность:<span>${filmInfo.Runtime}</span></p>
+            <p class="film__description">Жанр:<span>${filmInfo.Genre}</span></p>
+            <p class="film__description">Режиссер:<span>${filmInfo.Director}</span></p>
+            <p class="film__description">Сценарий:<span>${filmInfo.Writer}</span></p>
+            <p class="film__description">Актеры:<span>${filmInfo.Actors}</span></p>
+        </div>
     </div>
     <p class="film__plot">${filmInfo.Plot}</p>
     `;
